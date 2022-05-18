1bffce5347f71bffd7fa7f12af14859e04eb9711
kevin knight <tgodkev@gmail.com>
2022-05-18 01:45:47 -0400

    Reactive forms with validation completed.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
-import {FormControl, FormGroup} from '@angular/forms';
+import {FormArray, FormControl, FormGroup, Validators} from '@angular/forms';
+import {Observable} from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -11,12 +12,67 @@ export class AppComponent implements OnInit {
   // new property that will hold form in the end. a form in angular is a group of controls.
   signupForm: FormGroup;
 
+  // forbidden usernames for custom validator example
+  forbiddenUsernames = ['Chris', 'Anna'];
+
   ngOnInit() {
     this.signupForm = new FormGroup({
       // creating the form controls.
-      'username': new FormControl(null),
-      'email': new FormControl(null),
-      'gender' : new FormControl('male'),
+      // second argument in formControl is for validation.
+      'userData': new FormGroup({
+        'username': new FormControl(null, [Validators.required, this.forbiddenNames.bind(this)]),
+        'email': new FormControl(null, [Validators.required, Validators.email], this.forbiddenEmails),
+      }),
+      'gender': new FormControl('male'),
+      'hobbies': new FormArray([])
     });
+    this.signupForm.valueChanges.subscribe(
+      (value) => {
+        console.log(value);
+      }
+    );
   }
+
+  // form submit method
+
+  onSubmit() {
+    console.log(this.signupForm);
+  }
+
+  // method for on add hobby button.
+  onAddHobby() {
+    const control = new FormControl(null, Validators.required);
+    (<FormArray>this.signupForm.get('hobbies')).push(control);
+  }
+
+  // getting hobbies for ngFor loop.
+  getControls() {
+    return (<FormArray>this.signupForm.get('hobbies')).controls;
+  }
+
+  // new custom validator.
+  forbiddenNames(control: FormControl): { [s: string]: boolean } {
+    // if statement to check if username is forbidden.
+    // if validation passes must return null
+    if (this.forbiddenUsernames.indexOf(control.value) !== -1) {
+      return {'nameIsForbidden': true};
+    }
+    return null;
+  }
+
+  // asycn validator
+
+  forbiddenEmails(control: FormControl): Promise<any> | Observable<any> {
+    const promise = new Promise<any>((resolve, reject) => {
+      setTimeout(() => {
+        if (control.value === 'test@test.com') {
+          resolve({'emailIsForbidden': true});
+        } else {
+          resolve(null);
+        }
+      }, 1500);
+    });
+    return promise;
+  }
+
 }
